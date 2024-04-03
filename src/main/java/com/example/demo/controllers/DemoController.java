e8e0cacea6205502bcefb41b53289bc5026c0068
LUIS ERNESTO MUNAR RENGIFO <munar@MacBook-Pro-de-LUIS.local>
2024-04-03 11:25:21 -0500

    Video 290 ready

--- src/main/java/com/example/demo/controllers/DemoController.java
+++ src/main/java/com/example/demo/controllers/DemoController.java
@@ -1,13 +1,27 @@
 package com.example.demo.controllers;
 
 import org.springframework.stereotype.Controller;
+// import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/app")
 public class DemoController {
-  @RequestMapping(value={"/", "/home"}, method=RequestMethod.GET)
-  public String hello() {
-    return "hello";
+  @RequestMapping(value={"", "/", "/regards"}, method=RequestMethod.GET)
+  public ModelAndView hello(ModelAndView mv) {
+    mv.addObject("title", "Spring Boot");
+    mv.addObject("regards", "Hey from spring boot!");
+    mv.setViewName("hello");
+    return mv;
   }
+
+  // Other case, the most common
+  // @RequestMapping(value={"", "/", "/regards"}, method=RequestMethod.GET)
+  // public String hello(Model model) {
+  //   model.addAttribute("title", "Spring Boot");
+  //   model.addAttribute("regards", "Hey from spring boot!");
+  //   return "hello";
+  // }
 }

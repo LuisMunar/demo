7f90284f084adb07ae4633cdcb4d584ca487fac4
LuisMunar <luis.emunarr@gmail.com>
2024-04-03 19:32:40 -0500

    Video 298 ready

--- src/main/java/com/example/demo/controllers/PathParametersController.java
+++ src/main/java/com/example/demo/controllers/PathParametersController.java
@@ -9,10 +9,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/path-parameters")
 public class PathParametersController {
-  @GetMapping("/{id}") 
-  public String main(@PathVariable int id, Model model) {
-    model.addAttribute("title", "Path Parameters");
-    model.addAttribute("id", id);
+  @GetMapping("")
+  public String list(Model model) {
+    model.addAttribute("title", "List to Path Parameters");
     return "path-parameters/index";
+  }
+
+  @GetMapping("/single/{id}") 
+  public String singlePathParameter(@PathVariable int id, Model model) {
+    model.addAttribute("title", "Single Path Parameters");
+    model.addAttribute("id", id);
+    return "path-parameters/single";
+  }
+
+  @GetMapping("/multiple/{name}/{email}")
+  public String multiplePathParameters(@PathVariable String name, @PathVariable String email, Model model) {
+    model.addAttribute("title", "Multiple Path Parameters");
+    model.addAttribute("name", name);
+    model.addAttribute("email", email);
+    return "path-parameters/multiple";
   }
 }
